3ecce1dc390826241bf8afb1823c7c4b99b0dec4
Matt <greenwoodmatthew49@gmail.com>
2021-11-10 17:03:49 +0100

    Cleaning and editing of the ecological data. General improvements to code aesthetics.

--- Data wrangling/coenonympha_occurence_data_pruning.R
+++ Data wrangling/coenonympha_occurence_data_pruning.R
@@ -1,10 +1,11 @@
 rm(list=ls())
 
-# this cleaning process largely follows http://rstudio-pubs-static.s3.amazonaws.com/415459_61fae98ff3984241bd5f7f31da425c98.html#download-occurrence-data-from-gbif
+# this data cleaning process largely follows http://rstudio-pubs-static.s3.amazonaws.com/415459_61fae98ff3984241bd5f7f31da425c98.html#download-occurrence-data-from-gbif
 
-######################
-## LOADING PACKAGES ##
-######################
+
+#------------------------#
+#### Loading Packages ####
+#------------------------#
 
 #install the pacman package manager if necessary
 if (!requireNamespace("pacman", quietly = TRUE))
@@ -20,9 +21,10 @@ pacman::p_load(here, # to provide document paths relative to the R project
                scrubr,
                spThin)
 
-#######################################
-## DOWNLOAD AND CLEAN DATA FROM GBIF ##
-#######################################
+
+#------------------------------------------------#
+#### Downloading and Preparing Data from GBIF ####
+#------------------------------------------------#
 
 # GBIF data (search term "Coenonympha") downloaded at https://doi.org/10.15468/dl.xprfux; here, the unzipped Darwin Core Archive file is placed straight into the Raw data folder and used
 
@@ -42,51 +44,29 @@ c_data <- c_data %>%
          !is.na(decimalLatitude), # only retain rows with provided latitude coordinates
          species!='', # remove rows with no species-level taxonomic assignments
          !occurrenceStatus %in% c("absent", "Absent", "ABSENT", "ausente", "Ausente", "AUSENTE"), # remove any entries with absent notes
-         coordinateUncertaintyInMeters <= (5000) # set maximum coordinate uncertainty for an arbitrary but reasonable grid value like 5km
+         coordinateUncertaintyInMeters <= 5000 # set maximum coordinate uncertainty for an arbitrary but reasonable grid value like 5km
          #decimalLatitude > 0, decimalLongitude > -30 # can be used to constrain coordinates to, e.g., the Palearctic
          )
 
 # Clean the data further with functions of the 'scrubr' package
 c_data <- coord_incomplete(coord_imprecise(coord_impossible(coord_unlikely(c_data))))
 
-# Thin the data points to reduce geographic sampling bias. Here, the previously selected aribtrary uncertainty theshold (5km) is used
-c_data.thin <- thin(c_data, 
-                        verbose = FALSE, 
-                        lat.col = "decimalLatitude",
-                        long.col = "decimalLongitude",
-                        spec.col = "species",
-                        thin.par = 5,
-                        reps = 100, 
-                        write.files = FALSE,
-                        write.log.file = FALSE,
-                        locs.thinned.list.return = TRUE)
-
-final_occurrence_data <- final_occurrence_data %>%
-  group_by(species) %>%
-  filter(between(decimalLatitude, mean(decimalLatitude, na.rm=TRUE) - (2.5 * sd(decimalLatitude, na.rm=TRUE)), 
-                 mean(decimalLatitude, na.rm=TRUE) + (2.5 * sd(decimalLatitude, na.rm=TRUE)))) %>%
-  filter(between(decimalLongitude, mean(decimalLongitude, na.rm=TRUE) - (2.5 * sd(decimalLongitude, na.rm=TRUE)), 
-                 mean(decimalLongitude, na.rm=TRUE) + (2.5 * sd(decimalLongitude, na.rm=TRUE))))
-
-# Removing dense geographic data which may be an indicator of bias - using a avg distance of 5 km between samples - arbitrary 
-# see https://rmacroecology.netlify.app/2019/01/21/niche-overlap-update-to-silva-et-al-2014-supplementary-matterial/
+# In a later script, one occurrence point per raster cell will be selected to avoid geographic sampling bias
 
 
-##############################
-## QUICK DATA VISUALIZATION ##
-##############################
+#---------------------------------#
+#### Simple Data Visualization ####
+#---------------------------------#
 
 map("world", xlim = range(c_data$decimalLongitude), ylim = range(c_data$decimalLatitude))  # if the map doesn't appear right at first, run this command again
 points(c_data[ , c("decimalLongitude", "decimalLatitude")], pch = ".")
 
 
-#########################
-## EXPORT CLEANED DATA ##
-#########################
+#------------------------------#
+#### Exporting Cleaned Data ####
+#------------------------------#
 
-c_data <- c_data %>% 
-  dplyr::arrange(species)
-
-write.csv2(c_data, here("./Data wrangling/Processed data/coenonympha_occurrence_data.csv"))
-
-#end of file
+write.csv2(c_data, 
+           here("./Data wrangling/Processed data/coenonympha_occurrence_data.csv"),
+           row.names=FALSE
+           )
--- Data wrangling/pulling_ecological_variables.R
+++ Data wrangling/pulling_ecological_variables.R
@@ -1,75 +1,195 @@
 rm(list=ls())
 
-setwd('C:/Users/Matt/Documents/Coenonympha_Data/Results')
+
+# LOOK AT GRIDSAMPLE FOR GEOGRAPHIC SUBSAMPLING
 
 
-# from https://rspatial.org/raster/sdm/4_sdm_envdata.html
+# This data preparation process largely follows:
+#   https://rspatial.org/raster/sdm/4_sdm_envdata.html
 
-library(rgdal)
-library(dismo)
-library(raster)
-library(sf)
-library(dplyr)
-library(spThin)
-library(tidyverse)
+#------------------------#
+#### Loading Packages ####
+#------------------------#
 
+#install the pacman package manager if necessary
+if (!requireNamespace("pacman", quietly = TRUE))
+  install.packages("pacman")
 
-#path to the folder containing the bioclim variables
-pathb <- file.path('C:/Users/Matt/Documents/Coenonympha_Data/BioClim/2.5m/bioclim')
-biofiles <- list.files(pathb, pattern='bil$', full.names=TRUE)
-biofiles
+pacman::p_load(here, # to provide document paths relative to the R project
+               rgdal, # for the fread function
+               dismo, # for the gridSample function
+               raster,
+               sf,
+               dplyr,
+               spThin,
+               tidyverse,
+               sf, # for reading in shape files - st_read()
+               fasterize,
+               ggplot2, # for graph plotting
+               )
 
-#path to the folder containing the elevation data
-pathe <- file.path('C:/Users/Matt/Documents/Coenonympha_Data/BioClim/2.5m/elev')
-elefiles <- list.files(pathe, pattern='tif$', full.names=TRUE)
-elefiles
+#---------------------------------#
+#### Reading in Worldclim Data ####
+#---------------------------------#
+
+climate <- getData('worldclim',
+                   var='bio',
+                   path=here("./Data wrangling/Raw data/Bioclim/"),
+                   res=2.5
+                   )
+
+altitude <- getData('worldclim',
+                    var='alt',
+                    path=here("./Data wrangling/Raw data/Bioclim/"),
+                    res=2.5
+                    )
 
 
 predictorsb <- stack(biofiles)
 predictorse <- stack(elefiles)
 
-# Check alignment of predictor maps - keep in mind occurence data matched wrld simpl perfectly
-#library(maptools)
-#data(wrld_simpl)
-#plot(predictorsb,1)
-#plot(wrld_simpl, add=TRUE)
-#points(occur[ , c("decimalLongitude", "decimalLatitude")], pch = ".")
-#reading in a WWF terrestrial biomes classification file
-# following https://stackoverflow.com/questions/35096133/converting-shapefile-to-raster
-# and https://gis.stackexchange.com/questions/376280/converting-a-component-of-a-shapefile-into-a-geotiff-file-in-r
 
-#library(fasterize)
-#library(dplyr)
-#library(sf)
-#library(tidyverse)
+#------------------------------#
+#### Reading in Biome Data  ####
+#------------------------------#
 
-# Make a BIOM tif file first
-#biomefiles <- st_read('C:/Users/Matt/Documents/Coenonympha_Data/BioClim/official_teow/official/wwf_terr_ecos.shp', stringsAsFactors = F)
-# the raster cell size is 2.5m for the other data - therefore resolution is 2.5/60
-#raster(resolution = 2.5/60, crs = "+proj=longlat +datum=WGS84 +no_defs", vals = 0) %>% 
-#  fasterize(biomefiles, ., field = "BIOME") %>% 
-#  writeRaster(., "C:/Users/Matt/Documents/Coenonympha_Data/BioClim/official_teow/official/biomes.tif")
+# Unfortunately, adding a raster for categorical biome data is not as simple as
+#   as adding the worldcilm rasters. The biome file I am using can be retrieved
+#   from https://ecoregions.appspot.com by downloading the Ecoregions2017.zip.
+#   The downloaded file should be unzipped and placed into the
+#   "/Data wrangling/Raw data/" folder
 
-pathbiome <- file.path('C:/Users/Matt/Documents/Coenonympha_Data/BioClim/official_teow/official')
-biomefiles <- list.files(pathbiome, pattern='tif$', full.names=TRUE)
-biomefiles
+# The unzipped file we need is a shape file (.shp), which is converted to a 
+#   raster following guidelines stipulated in the tutorial below:
+#   https://gis.stackexchange.com/questions/376280/converting-a-component-of-a-shapefile-into-a-geotiff-file-in-r
 
-predictorsbiome <- raster(biomefiles)
+# The below check is used to avoid doing this conversion every time the script is run
+#   Essentially, we look for the name of the raster to be produced (Ecoregions2017.tif)
+#   and, if it already exists, skip the raster production steps
 
-#remove the 98 (lakes) and 99 (rock/ice) assignments
-predictorsbiome[predictorsbiome==98] <- NA
-predictorsbiome[predictorsbiome==99] <- NA
+if(file.exists(here("./Data wrangling/Raw data/Ecoregions2017/Ecoregions2017.tif")) == FALSE){
+  
+  # Read in the shape file
+  biomefile <- st_read(here("./Data wrangling/Raw data/Ecoregions2017/Ecoregions2017.shp"), stringsAsFactors = F)
+  
+  # Convert the shape file to a raster
+  # Note that the raster cell size is 2.5 arc minutes for the other raster data.
+  #   Therefore, the required resolution (in degrees) is (2.5 arc mins)/(60 arc mins)
+  raster(resolution = 2.5/60, 
+         crs = "+proj=longlat +datum=WGS84 +no_defs", 
+         vals = 0) %>%
+    fasterize(biomefile, ., field = "BIOME_NUM") %>%
+    writeRaster(., here("./Data wrangling/Raw data/Ecoregions2017/Ecoregions2017.tif"))
+  
+  rm(biomefile) # clean the biome shape file from memory
+  
+}else{
+  message("The converted raster file for the biome shape file already exists")
+}
+
+# Load in the prepared biome raster data
+biome <- raster(here("./Data wrangling/Raw data/Ecoregions2017/Ecoregions2017.tif"))
+
+
+#----------------------------------#
+#### Checking Raster Alignments ####
+#----------------------------------#
+
+# Check that the resolution of each map matches
+res(climate)
+res(altitude)
+res(biome)
+
+# Check that the extent of each map starts and ends in compatible locations
+extent(climate)
+extent(altitude)
+extent(biome) 
+# note that the ymin of biome is lower than the other datasets - the extents 
+#   should otherwise match perfectly
+
+# Alignments can also be checked by graphing the rasters over eachother with ggplot2
+# Just a small section of the rasters are plotted to inspect cell overlap, which
+#   is more challenging to observe when the entire raster is plotted
+plot(climate[[1]],
+     xlim=range(-5:10), # The x and y lims used are arbitrary
+     ylim=range(42:51),
+     alpha=0.3
+     )
+plot(biome, 
+     alpha=0.3,
+     xlim=range(-5:10),
+     ylim=range(42:51),
+     add=TRUE # ensures that this raster is placed above the first one
+     )
+
+# For the most part, the alignment is okay. The biome raster appears to generally
+#   fall a little short of the edges of the climate and altitude raster data
+#   (e.g., at xlim=range(-55:-50); ylim=range(48:51) or xlim=range(-5:10); ylim=range(48:51) )
+#   but the populated cells always overlap
+# It may be useful to look into cropping the original biome shapefile by the
+#   worldclim rasters for added accuracy (see, as a start, https://stackoverflow.com/questions/23073669/clipping-raster-using-shapefile-in-r-but-keeping-the-geometry-of-the-shapefile)
+
+
+#------------------------------------------------#
+#### Subsampling Occurrence Data with Rasters ####
+#------------------------------------------------#
+
+# There is almost certainly sampling bias in the occurrence dataset, with
+#   repeated ID of the same species in some regions (e.g., in close proximity 
+#   to urban regions) and sparse sampling in others (e.g. uninhabited locales)
+# To reduce the impact of this "geographic sampling bias" on the occurrence data,
+#   the occurrence points are resampled to ensure that, for each taxon, presence
+#   within an environmental raster cell is only counted once.
+# Notably, as all the raster cells are aligned, any can be used for this 
+#   the following occurrence point subsampling procedure
+
+# Pull the previously pruned occurrence data for Coenonympha
+occur <- read.csv(here("./Data wrangling/Processed data/coenonympha_occurrence_data.csv"),
+                  sep=';',
+                  dec=','
+                  )
+
+# Because the subsampling procedure takes a lot of time, I am honing it with a
+#   randomly subsampled dataset 
+t_occur <- occur %>%
+  group_by(species) %>%
+  slice_sample(n=100) # slice_sample will subsample to 100 rows or the total row number if <100
+
+occur_points <- t_occur[,c(5,4)]
+  
+thinned_occur <- t_occur %>% 
+  group_by("species") %>% # ensure that sampling is done separately for each species
+  
+  gridSample(xy, altitude, n=1)
+
+                
+
+
+
+#--------------------------------------------------------------#
+#### Assigning Ecological Raster Data to Occurrence Records ####
+#--------------------------------------------------------------#
+
+
+
+
+
+
 
 # Read in the occurrence data 
-occur <- read.csv('final occurrence data.csv', sep=';', dec=',') #need to let R know that commas are the decimals in the original dataset - will convert to periods
-occur <- occur[,-1] #a dummy column of integers was added for some reason so I remove it
+# Need to indicate that commas are the decimals in this dataset - these will be converted to periods
+occur <- read.csv(here("./Data wrangling/Processed data/coenonympha_occurrence_data.csv"),
+                  sep=';',
+                  dec=','
+                  ) 
 
-occur_points <- occur[c(5,4)]
+# Pull only the latitude and longitude data from the file
+occur_points <- occur[c(5, 4)]# lon and lat seem to be flipped - check this in pruning script
 
-extracted_valuesb <- extract(predictorsb, occur_points)
+extracted_climate <- extract(climate, occur_points)
 #the extracted bios correspond to annual temp (1), annual precipitation (12) and seasonality in temp (4) and precipitation (15)
-extracted_valuese <- extract(predictorse, occur_points)
-extracted_valuesbiome <- extract(predictorsbiome, occur_points)
+extracted_altitude <- extract(predictorse, occur_points)
+extracted_biome <- extract(predictorsbiome, occur_points)
 
 #looking at points on rasters
 samp <- sample(nrow(occur), 50000)
